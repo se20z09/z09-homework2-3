a47aa7287d8bf9c89d8ab41a97f9835220551b93
Nick Garner <nrgarner@ncsu.edu>
2020-08-24 10:54:03 -0400

    Added start, clear_screen methods to match timm

--- code/gol_ruby/life.rb
+++ code/gol_ruby/life.rb
@@ -1,3 +1,9 @@
+def start
+  sleep( 1 )
+  clear_screen
+  life(50, 20, 0.619, 200 )
+end
+
 def life ( rows, cols, some, gens )
   population = new Array( rows * cols )
   population.each do |person|
@@ -11,6 +17,9 @@ def live ( population, rows, cols, gens )
   if gens < 1
     return 0
   end
+  # Reset screen for new gen
+  sleep( 0.1 )
+  clear_screen
   # Print status of current gen
   printf( "Generation %3d\n", gens )
   count = 0
@@ -45,5 +54,9 @@ def live ( population, rows, cols, gens )
   live(next_gen, rows, cols, gens - 1)
 end
 
+def clear_screen
+  system( "clear" ) || system( "cls" )
+end
+
 # Start simulation
-life(50, 20, 0.619, 200 )
+start
